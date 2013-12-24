fdef5902ce3bb8d53264dbb7fa033b10587dd28d
Brian B <bbarnes@paragusit.com>
2013-12-24 15:05:40 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarLog::Application.config.secret_key_base = 'c15232da2ce05688eb5a9d183f3faa74668e222616ef29d269c8194ec4bd65cdbcbcb31b7bf11e257da8e5a8bbd0b38c6b26fde9ec9411fc01182cb2dd290677'
