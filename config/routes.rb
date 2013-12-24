6550474f12f6835047cab6e2807f9a7678e9d27e
Brian B <bbarnes@paragusit.com>
2013-12-24 16:45:24 -0500

    Create DB Structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 CarLog::Application.routes.draw do
+  resources :checks
+
+  resources :cars
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
